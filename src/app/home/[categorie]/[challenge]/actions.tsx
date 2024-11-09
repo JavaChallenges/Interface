730a73c20ec272d281381750dfc25286803a2ee6
Yannic Hock <yannic.hock@dfki.de>
2024-11-09 11:30:04 +0100

    Errormessages überarbeitet und sollten nun alle gut lesbar ausgewertet werden. Es konnte zusätzlich der "compile" step eingespart werden, wodurch sich die Wartezeit verkürzt

--- src/app/home/[categorie]/[challenge]/actions.tsx
+++ src/app/home/[categorie]/[challenge]/actions.tsx
@@ -38,8 +38,7 @@ export async function validateCode(
     try {
         const challengepath = "./challenges/" + formData.get("challengepath");
         copyTestFiles(challengepath, path.join("./workspace/", uuid));
-        await compile(uuid, classes );
-        const {statuscode, tests} = await test(uuid);
+        const {statuscode, tests} = await compileAndTest(uuid, classes);
 
         revalidatePath("/[categorie]/[challenge]");
         await deleteFolderRecursive(path.join("./workspace/", uuid));
@@ -68,9 +67,16 @@ function getErrorPositionsFromErrormessage(errormessage: string) {
     const matches = [...errormessage.matchAll(regex)];
 
     if (matches) {
-        const fileName = matches.map(m => m[1]);
-        const lineNumber = matches.map(m => {return parseInt(m[2])-1});
-        errorLines[fileName[0]] = [lineNumber[0]];
+        for (const index in matches) {
+            const match = matches[index];
+            const fileName = match[1];
+            const lineNumber = parseInt(match[2])-1;
+            if(errorLines[fileName]) {
+                errorLines[fileName].push(lineNumber);
+            } else {
+                errorLines[fileName] = [lineNumber];
+            }
+        }
     }
     return errorLines;
 }
@@ -89,13 +95,18 @@ async function deleteFolderRecursive(path: string) {
     }
 }
 
-async function test(uuid: UUID): Promise<{statuscode: number, tests: {name: string, failtype?: string, failmessage?: string}[]}> {
-    return new Promise((resolve) => {
-        exec(`mvn test -f ./workspace/${uuid}/pom.xml`, () => {
+async function compileAndTest(uuid: UUID, classes: {name: string, content: string}[]): Promise<{statuscode: number, tests: {name: string, failtype?: string, failmessage?: string}[]}> {
+    copySourceFiles(uuid, classes);
+    return new Promise((resolve, reject) => {
+        exec(`mvn test -f ./workspace/${uuid}/pom.xml`, (error, stdout) => {
             const tests: {name:string, failtype?:string, failmessage?:string}[] = []
             let testamount = 0;
             let failed = 0;
-            
+
+            if (error && stdout.includes("COMPILATION ERROR")) {
+                reject(`${filterErrorMessages(stdout)}`);
+                return;
+            }
             
             fs.readdirSync(`./workspace/${uuid}/target/surefire-reports`).forEach(file => {
                if(file.endsWith('.xml')) {
@@ -127,8 +138,22 @@ async function test(uuid: UUID): Promise<{statuscode: number, tests: {name: stri
     });
 }
 
+function filterErrorMessages(errormessage: string):string {
+    /*
+        Clean up the default maven error message to only show the most relevant information to the user
+    */
+    let regex = /\[INFO] -*\s*\[ERROR] COMPILATION ERROR :\s*\[INFO] -*\s*\[ERROR] [\S\s]*\[INFO] \d errors?\s*\[INFO] -*/gm;
+    errormessage = errormessage.match(regex)?.join("\n") ?? errormessage;
+    regex = /\/.*\/src\//mg
+    errormessage = errormessage.replaceAll(regex, "");
+    regex = /\[[A-Z]*] /mg
+    errormessage = errormessage.replaceAll(regex, "");
+    regex = /.*[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}\/tests\/.*\.java:\[\d*,\d*]/gm
+    errormessage = errormessage.replaceAll(regex, "");
+    return errormessage
+}
 
-async function compile(uuid: UUID, classes: {name: string, content: string}[]) {
+function copySourceFiles(uuid: string, classes: {name: string, content: string}[]) {
     const folderPath = path.join("./workspace/", uuid + "/src/");
     if (!fs.existsSync(folderPath)) {
         fs.mkdirSync(folderPath, { recursive: true });
@@ -137,26 +162,6 @@ async function compile(uuid: UUID, classes: {name: string, content: string}[]) {
     classes.map((jclass) => {
         const filePath = path.join(folderPath, jclass.name + '.java');
         fs.writeFileSync(filePath, jclass.content, 'utf8');
-    });
-
-    return new Promise((resolve, reject) => {
-        exec(`mvn compile -f ./workspace/${uuid}/pom.xml`, (error, stdout, stderr) => {
-            if (error) {
-                /*
-                    Clean up the default maven error message to only show the most relevant information to the user
-                */
-
-                let regex = /\[INFO] -*\s*\[ERROR] COMPILATION ERROR :\s*\[INFO] -*\s*\[ERROR] .*\s*\[INFO] .*\s*\[INFO] -*/mg
-                stdout = stdout.match(regex)?.join("\n") ?? stdout;
-                regex = /\/.*\/src\//mg
-                stdout = stdout.replaceAll(regex, "");
-                regex = /\[[A-Z]*] /mg
-                stdout = stdout.replaceAll(regex, "");
-                reject(`${stdout}`);
-                return;
-            }
-            resolve(`stdout: ${stdout}\nstderr: ${stderr}`);
-        });
     });
 }
 
